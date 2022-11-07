8c97bd8dc6a931a35ce9b0922c3b79ee59328dfe
EniseK <enisekizilaslan@gmail.com>
2022-11-07 13:13:17 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<h1> Weather App</h1>
-			<footer>
-				This project was coded by{" "}
-				<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
-					Enise Kizilaslan
-				</a>{" "}
-				and is {""}
-				<a href="https://github.com/EniseK/react-weather-app" target="_blank">
-					open sourced on GitHub.
-				</a>
-			</footer>
+			<div className="container">
+				<h1> Weather App</h1>
+
+				<footer>
+					This project was coded by{" "}
+					<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
+						Enise Kizilaslan
+					</a>{" "}
+					and is {""}
+					<a href="https://github.com/EniseK/react-weather-app" target="_blank">
+						open sourced on GitHub.
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
