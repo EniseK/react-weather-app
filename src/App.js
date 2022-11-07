058e5f79c5dfe694d54720d1a8603a89fc2ea783
EniseK <enisekizilaslan@gmail.com>
2022-11-07 12:23:36 +0100

    Added the GitHub Repo URL and link to personal page.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			<h1> Weather App</h1>
+			<footer>
+				This project was coded by{" "}
+				<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
+					Enise Kizilaslan
+				</a>{" "}
+				and is {""}
+				<a href="https://github.com/EniseK/react-weather-app" target="_blank">
+					open sourced on GitHub.
+				</a>
+			</footer>
+		</div>
+	);
 }
