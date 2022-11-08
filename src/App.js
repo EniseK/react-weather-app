b818f087c52cbb899e410601f7b17938bc708f15
EniseK <enisekizilaslan@gmail.com>
2022-11-08 11:25:22 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
 	return (
 		<div className="App">
-			<div className="container">
-				<Weather />
-				<footer>
-					This project was coded by{" "}
-					<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
-						Enise Kizilaslan
-					</a>{" "}
-					and is {""}
-					<a href="https://github.com/EniseK/react-weather-app" target="_blank">
-						open sourced on GitHub.
-					</a>
-				</footer>
-			</div>
+			<Weather />
+			<footer>
+				This project was coded by{" "}
+				<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
+					Enise Kizilaslan
+				</a>{" "}
+				and is {""}
+				<a href="https://github.com/EniseK/react-weather-app" target="_blank">
+					open sourced on GitHub.
+				</a>
+			</footer>
 		</div>
 	);
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
 							type="search"
 							placeholder="Type city name..."
 							className="form-control"
+							autoFocus="on"
 						/>
 					</div>
 					<div className="col-3">
-						<input type="submit" value="Search" className="btn btn-primary" />{" "}
+						<input
+							type="submit"
+							value="Search"
+							className="btn btn-primary w-100"
+						/>{" "}
 					</div>
 				</div>
 			</form>
@@ -24,13 +29,19 @@ export default function Weather() {
 				<li>Cloudy</li>
 			</ul>
 
-			<div className="row">
+			<div className="row mt-3">
 				<div className="col-6">
-					<img
-						src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-						alt="Cloudy"
-					></img>{" "}
-					14 °C
+					<span className="clearfix">
+						<img
+							src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+							alt="Cloudy"
+							className="float-left"
+						></img>
+						<span className="float-left">
+							<span className="temperature">14</span>
+							<span className="unit">°C</span>
+						</span>
+					</span>
 				</div>
 				<div className="col-6">
 					<ul>
