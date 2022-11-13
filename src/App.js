c036913e173b65c5e4c7bc50e3ce6e746e30baa9
EniseK <enisekizilaslan@gmail.com>
2022-11-13 15:18:44 +0100

    Fixed a few small issues

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ export default function App() {
 			<Weather defaultCity="Hannover" />
 			<footer>
 				This project was coded by{" "}
-				<a href="https://classy-empanada-a7c461.netlify.app/" target="_blank">
+				<a
+					href="https://classy-empanada-a7c461.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
 					Enise Kizilaslan
 				</a>{" "}
 				and is {""}
-				<a href="https://github.com/EniseK/react-weather-app" target="_blank">
+				<a
+					href="https://github.com/EniseK/react-weather-app"
+					target="_blank"
+					rel="noreferrer"
+				>
 					open sourced on GitHub.
 				</a>
 			</footer>
