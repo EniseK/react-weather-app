65b583026d0095ed3aebf964f0f57bf35d15eb65
EniseK <enisekizilaslan@gmail.com>
2022-11-21 15:23:10 +0100

    Working on multiple forecast days

--- src/Weather.js
+++ src/Weather.js
@@ -22,6 +22,7 @@ export default function Weather(props) {
 			iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
 		});
 	}
+
 	function handleSubmit(event) {
 		event.preventDefault();
 		search();
@@ -62,7 +63,6 @@ export default function Weather(props) {
 						</div>
 					</div>
 				</form>
-
 				<h1>{weatherData.city}</h1>
 				<ul>
 					<li>
@@ -70,7 +70,6 @@ export default function Weather(props) {
 					</li>
 					<li className="text-capitalize">{weatherData.description}</li>
 				</ul>
-
 				<div className="row mt-3">
 					<div className="col-6">
 						<span className="clear-fix">
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,10 +5,8 @@ import axios from "axios";
 export default function WeatherForecast(props) {
 	const [loaded, setLoaded] = useState(false);
 	const [forecastData, setForecastData] = useState({ loaded: false });
-
 	function handleResponse(response) {
 		setForecastData(response.data.daily);
-
 		setLoaded(true);
 	}
 
@@ -19,25 +17,28 @@ export default function WeatherForecast(props) {
 			let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 			return days[day];
 		}
+
 		return (
 			<div className="WeatherForecast">
 				<div className="row">
 					<div className="col">
-						<div className="WeatherForecast-day">{day()}</div>
-						<img
-							src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastData[0].condition.icon}.png`}
-							alt="Forecast"
-						></img>
-					</div>{" "}
-					<div className="WeatherForecast-temperatures">
-						<span className="WeatherForecast-temp-max">
-							{" "}
-							{Math.round(forecastData[0].temperature.maximum)}°
-						</span>{" "}
-						/{" "}
-						<span className="WeatherForecast-temp-min">
-							{Math.round(forecastData[0].temperature.minimum)}°
-						</span>
+						<div className="WeatherForecast-day">{day()}</div>{" "}
+						<div>
+							<img src={""} alt="Forecast">
+								src=
+								{`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastData[0].condition.icon}.png`}
+							</img>
+						</div>{" "}
+						<div className="WeatherForecast-temperatures">
+							<span className="WeatherForecast-temp-max">
+								{" "}
+								{Math.round(forecastData[0].temperature.maximum)}°
+							</span>{" "}
+							/{" "}
+							<span className="WeatherForecast-temp-min">
+								{Math.round(forecastData[0].temperature.min)}°
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
